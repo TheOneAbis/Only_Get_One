6787b5f094ab1b1f4fac86f28aaf44151661504d
Jared Goronkin <jgoronkin@gmail.com>
2025-05-25 13:08:06 -0400

    Score Screen

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 public static class Data
 {
     public static int Points;
-    public static HashSet<string> Goals;
+    public static HashSet<string> Goals = new();
 
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,63 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text _scoreText;
+    [SerializeField]
+    TMP_Text _goalsText;
+
+    [SerializeField]
+    List<string> _allGoals;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(Display());
+        Data.Points = 100;
+        Data.Goals.Add("test1");
+        Data.Goals.Add("test2");
+        Data.Goals.Add("test3");
+    }
+
+    IEnumerator Display()
+    {
+        yield return new WaitForSeconds(0.75f);
+
+        float scoreDelay = 0.5f;
+        for (int i = 0; i < Data.Points; i++)
+        {
+            _scoreText.text = "Coins Found: " + i;
+            //scoreDelay *= 0.9f;
+            yield return new WaitForSeconds(scoreDelay * (Mathf.Pow(1.0f - (float)i/ (float)Data.Points, 4))); 
+        }
+
+        yield return new WaitForSeconds(2.3f);
+
+        float goalDelay = 1.4f;
+        _goalsText.text = "Checkpoints: ";
+        for (int i = 0; i < _allGoals.Count; i++)
+        {
+            _goalsText.text += '\n';
+            if (Data.Goals.Contains(_allGoals[i]))
+            {
+                _goalsText.text += "<color=green> " + _allGoals[i];
+            }
+            else
+            {
+                _goalsText.text += "<color=red> " + _allGoals[i];
+            }
+            //goalDelay *= 0.65f;
+            yield return new WaitForSeconds(goalDelay);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
